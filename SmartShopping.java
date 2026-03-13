4eed2bc719b248fa7de722098cdf98aae0e2da8b
JOSE_VILLAR <villar20052@gmail.com>
2026-03-12 20:15:22 -0500

    ya importamos el scanner y el arraylist, seguimos trabajando el codigo...

--- SmartShopping.java
+++ SmartShopping.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SmartShopping {
 
   public static void main(String[] args) {
+     Scanner keyboard = new Scanner(System.in);
+    ArrayList shoppingList = new ArrayList<>();
 
+      shoppingList.add("")
   }
+
 }
